7cb6be477b3bc629b4557e805c5c340df94b2ae4
Deima <deima.balciute@gmail.com>
2023-01-20 15:29:18 +0200

    done

--- suma.cpp
+++ suma.cpp
@@ -1,1 +1,20 @@
-cc
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a, b, m;
+
+cin >> a;
+cin >> b;
+
+for(int i = a + 1;i < b; i++){
+
+    m += i;
+
+}
+
+    cout << m;
+
+    return 0;
+}
